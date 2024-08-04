a0cab16d19dd17be2d85a1fd6b6053556ced6e3c
NightFoxx254 <153968273+NightFoxx254@users.noreply.github.com>
2024-08-04 11:25:34 -0700

    Update Cribbage.py with the start to a counter for runs

--- Cribbage.py
+++ Cribbage.py
@@ -8,27 +8,73 @@ for i in range(4):
 hand.append(random.randint(1,10))
 print(hand)
 
+# #checking for pairs
+# for i in range(5):
+#     points+= (hand.count(hand[i])-1)
+# print(points)
+#
+# #checking for fifteens in two cards
+# for i in range(5):
+#     for j in range(5):
+#         if i!=j and hand[i]+hand[j] == 15:
+#             points+=(2/2)
+# print(points)
+#
+#
+# #checking for fifteens in three cards
+# for i in range(5):
+#     for j in range(5):
+#         for l in range(5):
+#             if i!=j and j!=l and hand[i]+hand[j]+hand[l] == 15:
+#                 points+=(2/3)
+# print(points)
+#
+# #checking for fifteens in four cards
+# for i in range(5):
+#     for j in range(5):
+#         for l in range(5):
+#             for k in range(5):
+#                 if i!=j and j!=l and l!=k and hand[i]+hand[j]+hand[l]+hand[k] == 15:
+#                     points+=(2/4)
+# print(points)
+#
+# #checkiong for fifteens in all the cards
+# if hand[0]+hand[1]+hand[2]+hand[3]+hand[4] == 15:
+#     points+=2
+
+#checking for runs of 3
+newHand = []
+smallNum = 11
+
 for i in range(5):
-    points+= (hand.count(hand[i])-1)
+    if hand[i] < smallNum:
+        smallNum = hand[i]
+        hand.pop(i)
+newHand.append(smallNum)
 
- 
-for i in range(5):
-    for j in range(5):
-        if i!=j and hand[i]+hand[j] == 15:
-            points+=(2/2)
+smallNum = 11
+for i in range(4):
+    if hand[i] < smallNum:
+        smallNum = hand[i]
+        hand.pop(i)
+newHand.append(smallNum)
 
-for i in range(5):
-    for j in range(5):
-        for l in range(5):
-            if i!=j and j!=l and hand[i]+hand[j]+hand[l] == 15:
-                points+=(2/3)
+smallNum = 11
+for i in range(3):
+    if hand[i] < smallNum:
+        smallNum = hand[i]
+        hand.pop(i)
+newHand.append(smallNum)
 
-for i in range(5):
-    for j in range(5):
-        for l in range(5):
-            for k in range(5):
-                if i!=j and j!=l and l!=k and hand[i]+hand[j]+hand[l]+hand[k] == 15:
-                    points+=(2/4)
+smallNum = 11
+for i in range(2):
+    if hand[i] < smallNum:
+        smallNum = hand[i]
+        hand.pop(i)
+newHand.append(smallNum)
+
+newHand.append(hand[0])
+hand.pop(0)
 
 
-print(points)
+print(newHand)
